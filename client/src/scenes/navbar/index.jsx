81b4c5034a75199426bf7a5be38f8db70c0a9ac9
Heorhii <gerythepatient@gmail.com>
2023-11-23 15:56:17 +0100

    started NavBar

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,4 +1,39 @@
+import { useState } from "react";
+import {
+    Box,
+    IconButton,
+    InputBase,
+    Typography,
+    Select,
+    MenuItem,
+    FormControl,
+    useTheme,
+    useMediaQuery
+} from "@mui/material";
+import {
+    Search,
+    Message,
+    DarkMode,
+    LightMode,
+    Notifications,
+    Help,
+    Menu,
+    Close
+} from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "state";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "components/FlexBetween";
+
 const Navbar = () => {
+    const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const user = useSelector((state) => state.user);
+    const isNonMobileScreens = useMediaQuery("(min-widht: 1000px)");
+
+    const theme = useTheme();
+
     return (<div>Navbar</div>)
 }
 
