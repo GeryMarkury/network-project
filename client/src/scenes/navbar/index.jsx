fce6dc9faa95236d84b725085ab79a9346cf1fb9
Heorhii <gerythepatient@gmail.com>
2023-11-24 21:32:36 +0100

    add mobile components to Navbar

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -33,8 +33,36 @@ const Navbar = () => {
     const isNonMobileScreens = useMediaQuery("(min-widht: 1000px)");
 
     const theme = useTheme();
+    const neutralLight = theme.palette.neutral.light;
+    const dark = theme.palette.neutral.dark;
+    const background = theme.palette.background.default;
+    const primaryLight = theme.palette.primary.light;
+    const alt = theme.palette.background.alt;
 
-    return (<div>Navbar</div>)
-}
+    const fullName = `${user.firstName} ${user.lastName}`;
+
+    return <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+        <FlexBetween gap="1.75rem">
+            <Typography fontWeight="bold" fontSize="clamp(1rem, 2rem, 2.25rem)" color="primary"
+                onClick={() => navigate("/home")}
+                sx={{
+                    "&:hover": {
+                        color: primaryLight,
+                        cursor: "pointer",
+                },
+                }}>
+                Network
+            </Typography>
+            {isNonMobileScreens && (
+                <FlexBetween backgroundColor={neutralLight} borderRadius="9px" gap="3rem" padding="0.1rem 1.5rem" >
+                    <InputBase placeholder="Search..." />
+                    <IconButton>
+                        <Search />
+                    </IconButton>
+                </FlexBetween>
+            )}
+        </FlexBetween>
+    </FlexBetween>;
+};
 
 export default Navbar;
