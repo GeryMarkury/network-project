c8dcb8de19e3e65341b08b7ee771f297bdc0d799
Heorhii <gerythepatient@gmail.com>
2023-11-27 17:48:06 +0100

    formatted docs

--- server/index.js
+++ server/index.js
@@ -32,15 +32,15 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "public/assets");
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    }
+	destination: function (req, file, cb) {
+		cb(null, "public/assets");
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname);
+	},
 });
 const upload = multer({ storage });
 
@@ -52,11 +52,14 @@ app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
 const PORT = process.env.PORT || 6001;
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    app.listen(PORT, () => console.log(`Server port: ${PORT}`));
-    // User.insertMany(users);
-    // Post.insertMany(posts);
-}).catch((error) => console.log(`${error} did not connect`));
+mongoose
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		app.listen(PORT, () => console.log(`Server port: ${PORT}`));
+		// User.insertMany(users);
+		// Post.insertMany(posts);
+	})
+	.catch(error => console.log(`${error} did not connect`));
