feec85b3022b5a8a6b013b5bd72ae23e81d5e158
Heorhii <gerythepatient@gmail.com>
2023-11-29 16:56:06 +0100

    continue Form

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -18,3 +18,67 @@ const registerSchema = yup.object().shape({
 	occupation: yup.string().required("required"),
 	picture: yup.string().required("required"),
 });
+
+const loginSchema = yup.object().shape({
+	email: yup.string().email("invalid email").required("required"),
+	password: yup.string().required("required"),
+});
+
+const initialValuesRegister = {
+	firstName: "",
+	lastName: "",
+	email: "",
+	password: "",
+	location: "",
+	occupation: "",
+	picture: "",
+};
+
+const initialValuesLogin = {
+	email: "",
+	password: "",
+};
+
+const Form = () => {
+	const [pageType, setPageType] = useState("login");
+	const { palette } = useTheme();
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const isNonMobile = useMediaQuery("(min-width: 600px)");
+	const isLogin = pageType === "login";
+	const isRegister = pageType === "register";
+
+	const handleFormSubmit = async (values, onSubmitProps) => {};
+
+	return (
+		<Formik
+			onSubmit={handleFormSubmit}
+			initialValues={isLogin ? initialValuesLogin : initialValuesRegister}
+			validationSchema={isLogin ? loginSchema : registerSchema}
+		>
+			{({
+				values,
+				errors,
+				touched,
+				handleBlur,
+				handleChange,
+				handleSubmit,
+				setFieldValue,
+				resetForm,
+			}) => (
+				<form onSubmit={handleSubmit}>
+					<Box
+						dissplay="grid"
+						gap="30px"
+						gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+						sx={{
+							"& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+						}}
+					></Box>
+				</form>
+			)}
+		</Formik>
+	);
+};
+
+export default Form;
