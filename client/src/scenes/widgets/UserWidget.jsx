df3307ed442f4cc5dc6c9cec5b71f69a72b859f7
Heorhii <gerythepatient@gmail.com>
2023-12-11 17:10:02 +0100

    add post widget

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -60,104 +60,108 @@ const UserWidget = ({ userId, picturePath }) => {
 						</Typography>
 						<Typography color={medium}>{friends.length} friends</Typography>
 					</Box>
-					<ManageAccountsOutlined />
 				</FlexBetween>
-				<Divider />
-				<Box p="1rem 0">
-					<Box
-						display="flex"
-						alignItems="center"
-						gap="1rem"
-						mb="0.5rem"
-					>
-						<LocationOnOutlined
-							fontSize="large"
-							sx={{ color: main }}
-						/>
-						<Typography color={medium}>{location}</Typography>
-					</Box>
-					<Box
-						display="flex"
-						alignItems="center"
-						gap="1rem"
-					>
-						<WorkOutlineOutlined
-							fontSize="large"
-							sx={{ color: main }}
-						/>
-						<Typography color={medium}>{occupation}</Typography>
-					</Box>
+				<ManageAccountsOutlined />
+			</FlexBetween>
+			<Divider />
+			<Box p="1rem 0">
+				<Box
+					display="flex"
+					alignItems="center"
+					gap="1rem"
+					mb="0.5rem"
+				>
+					<LocationOnOutlined
+						fontSize="large"
+						sx={{ color: main }}
+					/>
+					<Typography color={medium}>{location}</Typography>
 				</Box>
-				<Box p="1rem 0">
-					<FlexBetween mb="0.5rem">
-						<Typography color={medium}>Who's viewed your profile</Typography>
-						<Typography
-							color={main}
-							fontWeight="500"
-						>
-							{viewedProfile}
-						</Typography>
-					</FlexBetween>
-					<FlexBetween>
-						<Typography color={medium}>Impressions of your post</Typography>
-						<Typography
-							color={main}
-							fontWeight="500"
-						>
-							{impressions}
-						</Typography>
-					</FlexBetween>
+				<Box
+					display="flex"
+					alignItems="center"
+					gap="1rem"
+				>
+					<WorkOutlineOutlined
+						fontSize="large"
+						sx={{ color: main }}
+					/>
+					<Typography color={medium}>{occupation}</Typography>
 				</Box>
-				<Box p="1rem 0">
+			</Box>
+			<Divider />
+			<Box p="1rem 0">
+				<FlexBetween mb="0.5rem">
+					<Typography color={medium}>Who's viewed your profile</Typography>
 					<Typography
-						fontSize="1rem"
 						color={main}
 						fontWeight="500"
-						mb="1rem"
 					>
-						Social Profiles
+						{viewedProfile}
 					</Typography>
-					<FlexBetween
-						gap="1rem"
-						mb="0.5rem"
+				</FlexBetween>
+				<FlexBetween>
+					<Typography color={medium}>Impressions of your post</Typography>
+					<Typography
+						color={main}
+						fontWeight="500"
 					>
-						<FlexBetween gap="1rem">
-							<img
-								src="../assets/twitter.png"
-								alt="twitter"
-							/>
-							<Box>
-								<Typography
-									color={main}
-									fontWeight="500"
-								>
-									Twitter
-								</Typography>
-								<Typography color={medium}>Social Network</Typography>
-							</Box>
-						</FlexBetween>
-						<EditOutlined sx={{ color: main }} />
-					</FlexBetween>
+						{impressions}
+					</Typography>
+				</FlexBetween>
+			</Box>
+			<Divider />
+			<Box p="1rem 0">
+				<Typography
+					fontSize="1rem"
+					color={main}
+					fontWeight="500"
+					mb="1rem"
+				>
+					Social Profiles
+				</Typography>
+				<FlexBetween
+					gap="1rem"
+					mb="0.5rem"
+				>
 					<FlexBetween gap="1rem">
-						<FlexBetween gap="1rem">
-							<img
-								src="../assets/linkedin.png"
-								alt="linkedin"
-							/>
-							<Box>
-								<Typography
-									color={main}
-									fontWeight="500"
-								>
-									Linkedin
-								</Typography>
-								<Typography color={medium}>Network Platform</Typography>
-							</Box>
-						</FlexBetween>
-						<EditOutlined sx={{ color: main }} />
+						<img
+							src="../assets/twitter.png"
+							alt="twitter"
+						/>
+						<Box>
+							<Typography
+								color={main}
+								fontWeight="500"
+							>
+								Twitter
+							</Typography>
+							<Typography color={medium}>Social Network</Typography>
+						</Box>
 					</FlexBetween>
-				</Box>
-			</FlexBetween>
+					<EditOutlined sx={{ color: main }} />
+				</FlexBetween>
+				<FlexBetween gap="1rem">
+					<FlexBetween gap="1rem">
+						<img
+							src="../assets/linkedin.png"
+							alt="linkedin"
+						/>
+						<Box>
+							<Typography
+								color={main}
+								fontWeight="500"
+							>
+								Linkedin
+							</Typography>
+							<Typography color={medium}>Network Platform</Typography>
+						</Box>
+					</FlexBetween>
+					<EditOutlined sx={{ color: main }} />
+				</FlexBetween>
+			</Box>
 		</WidgetWrapper>
 	);
 };
+
+export default UserWidget;
