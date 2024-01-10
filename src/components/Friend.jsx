fe9dd564f84820626333d59b44704fcfe74400fe
Heorhii <hmarkov.dev@gmail.com>
2024-01-10 19:54:12 +0100

    remove add to friends icon from my posts

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -20,6 +20,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 	const medium = palette.neutral.medium;
 
 	const isFriend = friends.find(friend => friend._id === friendId);
+	const isMe = Boolean(friendId === _id);
 
 	const patchFriend = async () => {
 		const response = await fetch(
@@ -70,16 +71,18 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 					</Typography>
 				</Box>
 			</FlexBetween>
-			<IconButton
-				onClick={() => patchFriend()}
-				sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
-			>
-				{isFriend ? (
-					<PersonRemoveOutlined sx={{ color: primaryDark }} />
-				) : (
-					<PersonAddOutlined sx={{ color: primaryDark }} />
-				)}
-			</IconButton>
+			{!isMe && (
+				<IconButton
+					onClick={() => patchFriend()}
+					sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
+				>
+					{isFriend ? (
+						<PersonRemoveOutlined sx={{ color: primaryDark }} />
+					) : (
+						<PersonAddOutlined sx={{ color: primaryDark }} />
+					)}
+				</IconButton>
+			)}
 		</FlexBetween>
 	);
 };
