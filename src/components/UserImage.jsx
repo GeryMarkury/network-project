60098789fe068059459fc664326e1f8d3627485b
Heorhii <hmarkov.dev@gmail.com>
2024-01-10 15:16:37 +0100

    change the user image path

--- src/components/UserImage.jsx
+++ src/components/UserImage.jsx
@@ -11,7 +11,7 @@ const UserImage = ({ image, size = "60px" }) => {
 				width={size}
 				height={size}
 				alt="user"
-				src={`https://network-server-hmarkov.onrender.com/assets/${image}`}
+				src={image}
 			/>
 		</Box>
 	);
