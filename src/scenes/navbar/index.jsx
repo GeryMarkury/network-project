732ed69eb479fa30d400d64166bd4d48ba3de0d6
Heorhii <hmarkov.dev@gmail.com>
2024-01-12 16:59:33 +0100

    fix navbar

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
 	const user = useSelector(state => state.user);
-	const isNonMobileScreens = useMediaQuery("(min-widht: 1000px)");
+	const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
 
 	const theme = useTheme();
 	const neutralLight = theme.palette.neutral.light;
